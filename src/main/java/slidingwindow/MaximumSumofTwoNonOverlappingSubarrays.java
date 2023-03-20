8d0f32b63694f5622a530e1e914c76fd1e3eb328
Denis Rudenko <rdv2018s@gmail.com>
2023-03-20 16:13:45 +0300

    master

--- src/main/java/slidingwindow/MaximumSumofTwoNonOverlappingSubarrays.java
+++ src/main/java/slidingwindow/MaximumSumofTwoNonOverlappingSubarrays.java
@@ -39,20 +39,43 @@ package slidingwindow;
 public class MaximumSumofTwoNonOverlappingSubarrays {
 
     public static void main(String[] args) {
-        new MaximumSumofTwoNonOverlappingSubarrays().maxSumTwoNoOverlap(new int[]{0, 6, 5, 2, 2, 5, 1, 9, 4}, 1, 2);// 20
-        new MaximumSumofTwoNonOverlappingSubarrays().maxSumTwoNoOverlap(new int[]{3, 8, 1, 3, 2, 1, 8, 9, 0}, 3, 2);//
-        new MaximumSumofTwoNonOverlappingSubarrays().maxSumTwoNoOverlap(new int[]{2, 1, 5, 6, 0, 9, 5, 0, 3, 8}, 4, 3);//
+        System.out.println(new MaximumSumofTwoNonOverlappingSubarrays().maxSumTwoNoOverlap(new int[]{0, 6, 5, 2, 2, 5, 1, 9, 4}, 2, 1));// 20
+//        new MaximumSumofTwoNonOverlappingSubarrays().maxSumTwoNoOverlap(new int[]{3, 8, 1, 3, 2, 1, 8, 9, 0}, 3, 2);// 29
+//        new MaximumSumofTwoNonOverlappingSubarrays().maxSumTwoNoOverlap(new int[]{2, 1, 5, 6, 0, 9, 5, 0, 3, 8}, 4, 3);// 31
     }
 
 
     public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
-        int res = 0;
+        int windowsSum1 = 0, sum1res = 0, indexSum1Start = 0, windowsSum2 = 0, sum2res = 0;
+        int left = 0;
 
-        for (int i = 0 ; i < nums.length; i++) {
-            
+        for (int i = 0; i < nums.length; i++) {
+            sum1res += nums[i];
+
+            if (i + 1 >= firstLen) {
+                if (sum1res > windowsSum1) {
+                    windowsSum1 = sum1res;
+                    indexSum1Start = i - secondLen;
+                }
+                sum1res-=nums[left++];
+            }
+        }
+        for (int i =0; i < firstLen; i++) {
+            nums[indexSum1Start++] = Integer.MIN_VALUE;
         }
 
-        return 0;
+        left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum2res += nums[i];
+
+            if (i + 1 >= secondLen) {
+                windowsSum2 = Math.max(windowsSum2, sum2res);
+                sum2res-=nums[left++];
+            }
+        }
+
+        return windowsSum1 + windowsSum2;
 
     }
 }
